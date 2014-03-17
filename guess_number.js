56ff649444690ef3fbedf3642b9dc7e17f0f0e11
ndkerby <ndkerby@gmail.com>
2014-03-16 22:28:48 -0400

    Added a limit on the number of guesses allowed

--- guess_number.js
+++ guess_number.js
@@ -1,40 +1,47 @@
 calc_array = new Array();
 var number=Random();
+var guess_num = 3;
+
+window.onload = function(){ 
+    document.getElementById("num_result").value = "Guess a Number";
+    document.getElementById("guesses_left").value = guess_num; 
+}
 
 function Random() {
-
-return Math.floor(Math.random()*25);
+    return Math.floor(Math.random()*25);
 }
 
 function $id(id)
-{
-        return document.getElementById(id);
+{ 
+    document.getElementById("guesses_left").value = guess_num;
+    return document.getElementById(id);
 }
 function check_number(id)
 {
-    if(id == number){    
-        document.getElementById(id).style.backgroundColor="green";
-        console.dir("Found");
-        document.getElementById("num_result").value = "Congrats! You guessed the number!";
-    }
-    if(id > number){    
-        document.getElementById(id).style.backgroundColor="red";
-        for (var index = id ; index <= 25 ; index++){
-            document.getElementById(index).style.backgroundColor="red";
+    if(guess_num > 0){
+        guess_num--;
+        if(id == number){    
+            document.getElementById(id).style.backgroundColor="green";
+            console.dir("Found");
+            document.getElementById("num_result").value = "Congrats! You guessed the number!";
         }
-        console.dir("The number is lower");
-        document.getElementById("num_result").value = "The number is lower";
-    }
+        if(id > number){    
+            document.getElementById(id).style.backgroundColor="red";
+            for (var index = id ; index <= 25 ; index++){
+                document.getElementById(index).style.backgroundColor="red";
+            }
+            console.dir("The number is lower");
+            document.getElementById("num_result").value = "The number is lower";
+        }
     
-    if(id < number){    
-        document.getElementById(id).style.backgroundColor="red";
-        for (var index = id ; index > 0 ; index--){
-            document.getElementById(index).style.backgroundColor="red";
+        if(id < number){    
+            document.getElementById(id).style.backgroundColor="red";
+            for (var index = id ; index > 0 ; index--){
+                document.getElementById(index).style.backgroundColor="red";
+            }
+            console.dir("The number is higher");
+            document.getElementById("num_result").value = "The number is higher";
         }
-        console.dir("The number is higher");
-        document.getElementById("num_result").value = "The number is higher";
+        document.getElementById("guesses_left").value = guess_num;
     }
 }
-
-console.dir("Hello World");
-//console.dir(number);
