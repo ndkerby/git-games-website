7bbac533bbaad70e24ec794f5e7b4a331cbf716d
ndkerby <ndkerby@gmail.com>
2014-03-17 21:23:45 -0400

    Updated buttons so that the button is now disabled for eliminated numbers, all buttons now also become disabled when the user runs out of guesses. Lastly, added a "reset" button for when the user wants to replay the game or start over with a new number to guess

--- guess_number.js
+++ guess_number.js
@@ -1,6 +1,7 @@
 calc_array = new Array();
-var number=Random();
+var max = 25;
 var guess_num = 3;
+var number=Random();
 
 window.onload = function(){ 
     document.getElementById("num_result").value = "Guess a Number";
@@ -8,7 +9,7 @@ window.onload = function(){
 }
 
 function Random() {
-    return Math.floor(Math.random()*25);
+    return Math.floor(Math.random()*max);
 }
 
 function $id(id)
@@ -22,8 +23,9 @@ function check_number(id)
         guess_num--;
         if(id > number){    
             document.getElementById(id).style.backgroundColor="red";
-            for (var index = id ; index <= 25 ; index++){
+            for (var index = id ; index <= max ; index++){
                 document.getElementById(index).style.backgroundColor="red";
+                document.getElementById(index).disabled = true; 
             }
             console.dir("The number is lower");
             document.getElementById("num_result").value = "The number is lower";
@@ -33,20 +35,33 @@ function check_number(id)
             document.getElementById(id).style.backgroundColor="red";
             for (var index = id ; index > 0 ; index--){
                 document.getElementById(index).style.backgroundColor="red";
+                document.getElementById(index).disabled = true; 
             }
             console.dir("The number is higher");
             document.getElementById("num_result").value = "The number is higher";
         }
         if(guess_num > 0) {
             document.getElementById("guesses_left").value = guess_num;
-        } else {
+        } 
+        if(guess_num == 0) {
             document.getElementById("guesses_left").value = guess_num;
             document.getElementById("num_result").value = "No more guesses allowed...";
+            for (var index = max ; index > 0 ; index--){
+                document.getElementById(index).disabled = true; 
+            }
         }
         if(id == number){    
             document.getElementById(id).style.backgroundColor="green";
             console.dir("Found");
             document.getElementById("num_result").value = "Congrats! You guessed the number!";
+            for (var index = max ; index > 0 ; index--){
+                document.getElementById(index).disabled = true; 
+            }
         }
     }
+}
+
+function reset_game()
+{
+    window.location.reload();
 }
